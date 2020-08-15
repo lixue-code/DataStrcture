7a612501bb0e7ec6d8945fb356b036eb5a68fb0d
lixue <1760736397@qq.com>
2020-08-15 15:27:36 +0800

    队列

--- queue/src/com/test/MainTest.java
+++ queue/src/com/test/MainTest.java
@@ -1,6 +1,8 @@
 package com.test;
 
 import com.queue.ArrayQueue;
+import com.queue.CircleArrayQueue;
+import org.junit.Test;
 
 public class MainTest {
     public static void main(String[] args) {
@@ -17,6 +19,23 @@ public class MainTest {
         System.out.println("取出的数据："+data);
 
         arrayQueue.showQueue();
+
+    }
+
+    @Test
+    public void circleQueueTest(){
+        CircleArrayQueue circleArrayQueue = new CircleArrayQueue(4);
+        circleArrayQueue.addData(10);
+        circleArrayQueue.addData(20);
+        circleArrayQueue.addData(30);
+
+
+        circleArrayQueue.showQueue();
+        circleArrayQueue.getData();
+
+
+        circleArrayQueue.addData(40);
+        circleArrayQueue.showQueue();
 
     }
 }
